9ca511fd5d013d7f893c200ee0b89b063d6af5d3
Kevin <kevinpettinger@gmail.com>
2021-07-07 21:38:42 -0700

    feat: :sparkles: added back app mention reply

--- app.js
+++ app.js
@@ -3,8 +3,45 @@ const { createEventAdapter } = require('@slack/events-api');
 
 const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
 const slackToken = process.env.SLACK_TOKEN;
+const slackPort = process.env.SLACK_PORT || 3000;
 
 const slackEvents = createEventAdapter(slackSigningSecret);
-const slackClient = new WebClient(slackToken)
+const slackClient = new WebClient(slackToken);
 
-slackEvents.on()
+slackEvents.on('app_mention', (event) => {
+    console.log(`Got message from user ${event.user}: ${event.text}`);
+    (async () => {
+        console.log('sending ');
+        try {
+            const result = await slackClient.chat.postMessage({
+                channel: event.channel,
+                text: `HIIIII <@${event.user}>! :tada:`,
+            })
+            console.log(result);
+        } catch (error) {
+            console.log(error.data);
+        }
+    })();
+})
+
+// slackEvents.on('message.im', (event) => {
+//     console.log(`Got message from user ${event.user}: ${event.text}`);
+//     console.log(event);
+//     console.log(slackClient);
+//     (async () => {
+//         try {
+//             await slackClient.chat.postMessage({
+//                 channel: event.channel,
+//                 text: `HIIIII <@${event.user}>! :tada:`,
+//             })
+//         } catch (error) {
+//             console.log(error.data);
+//         }
+//     })
+// })
+
+slackEvents.on('error', console.error);
+
+slackEvents.start(slackPort).then(() => {
+    console.log(`Server started on port ${slackPort}.`);
+})
